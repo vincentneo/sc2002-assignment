e0cb127b56d614fa303924907748ff547cefff1b
jht2906 <jh88jiahao@gmail.com>
2025-03-26 16:59:42 +0800

    Update HDBManager.java

--- src/sc2002/FCS1/grp2/HDBManager.java
+++ src/sc2002/FCS1/grp2/HDBManager.java
@@ -1,22 +1,38 @@
 package sc2002.FCS1.grp2;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class HDBManager extends User {
 
+	Scanner scanner = new Scanner(System.in);
 	public HDBManager(String line) {
 		super(line);
 		
 	}
 
 	
-	
-	
 	public void createHDBProject() {
 		
-		System.out.println();
+		System.out.println("Enter Project Name: ");
+		String ProjectName = scanner.toString();
+		System.out.println("Enter Neighbourhood: ");
+		String Neighborhood = scanner.toString();
+		System.out.println("Enter Maximum two room units: ");
+		int maxTwoRoomUnits = scanner.nextInt();
+		System.out.println("Enter Maximum three room units: ");
+		int maxThreeRoomUnits = scanner.nextInt();
+		System.out.println("Enter Opening Date: ");
+		String openingDate = scanner.toString();
+		System.out.println("Enter Closing Date: ");
+		String closingDate = scanner.toString();
+		System.out.println("Enter number of officer slots: ");
+		int officerSlots = scanner.nextInt();
+		ArrayList<HDBOfficer> officers; //;
+		// to do officers
 		
-		BTOProject project = new BTOProject(projectName, neighborhood, maxTwoRoomUnits, maxThreeRoomUnits, openingDate, closingDate,  managerInCharge, officerSlots, officers);
+		
+		BTOProject project = new BTOProject(ProjectName, Neighborhood, maxTwoRoomUnits, maxThreeRoomUnits, openingDate, closingDate,  this, officerSlots, officers);
 		
 
 	}
