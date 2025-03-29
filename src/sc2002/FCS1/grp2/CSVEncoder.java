c5978e6b218eca7689f43dd1ae959b0a8c528877
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2025-03-30 00:55:15 +0800

    fix issue where csv written will lose header row

--- src/sc2002/FCS1/grp2/CSVEncoder.java
+++ src/sc2002/FCS1/grp2/CSVEncoder.java
@@ -1,9 +1,11 @@
 package sc2002.FCS1.grp2;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CSVEncoder {
 	private String path;
@@ -17,8 +19,19 @@ public class CSVEncoder {
 	}
 	
 	public void encode() throws IOException {
+		File file = new File(path);
+		Scanner reader = new Scanner(file);
+		
+		// get the header row from the current file.
+		String headerLine = reader.nextLine();
+		reader.close();
+		
 		FileWriter writer = new FileWriter(path);
 		BufferedWriter bufferedWriter = new BufferedWriter(writer);
+		
+		// write the header row as retrieved earlier.
+		bufferedWriter.write(headerLine);
+		bufferedWriter.newLine();
 		
 		for (CSVEncodable encodable : encodables) {
 			bufferedWriter.write(encodable.encode());
