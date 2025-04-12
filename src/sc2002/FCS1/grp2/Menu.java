ea62d36444e72f47452f42aceedb0e768a7f82eb
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2025-04-12 17:49:03 +0800

    Work in progress, menu builder

---
+++ src/sc2002/FCS1/grp2/Menu.java
@@ -0,0 +1,69 @@
+package sc2002.FCS1.grp2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+	private String title;
+	private List<String> descriptions;
+	private List<String> contents;
+	
+	private final String BOX_TOP_LEFT_CORNER = "┌";
+	private final String BOX_TOP_RIGHT_CORNER = "┐";
+	private final String BOX_BOTTOM_LEFT_CORNER = "└";
+	private final String BOX_BOTTOM_RIGHT_CORNER = "┘";
+	private final String LINE_HORIZONTAL = "─";
+	private final String LINE_VERTICAL = "│";
+	
+	private int maxWidth = 100;
+	
+	Menu(Builder builder) {
+		this.title = builder.title;
+		this.descriptions = builder.descriptions;
+		this.contents = builder.contents;
+	}
+	
+	public void display() {
+		System.out.print(asString());
+	}
+	
+	public String asString() {
+		String text = "";
+		
+		return text;
+	}
+	
+	public static class Builder {
+		private String title;
+		private List<String> descriptions;
+		private List<String> contents;
+		
+		public Builder setTitle(String title) {
+			this.title = title;
+			return this;
+		}
+		
+		public Builder addDescription(String description) {
+			if (descriptions == null) {
+				descriptions = new ArrayList<String>();
+			}
+			
+			descriptions.add(description);
+			return this;
+		}
+		
+		public Builder addContent(String content) {
+			if (contents == null) {
+				contents = new ArrayList<String>();
+			}
+			
+			contents.add(content);
+			return this;
+		}
+		
+		public Menu build() {
+			return new Menu(this);
+		}
+		
+	}
+}
