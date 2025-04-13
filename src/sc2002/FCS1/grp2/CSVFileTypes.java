61362de15053abe71d8fd48ea65a24b138f566b1
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2025-04-13 11:48:57 +0800

    Hardcode CSV file's header into program

--- src/sc2002/FCS1/grp2/CSVFileTypes.java
+++ src/sc2002/FCS1/grp2/CSVFileTypes.java
@@ -16,4 +16,15 @@ public enum CSVFileTypes {
 		return fileName;
 	}
 	
+	public String getHeader() {
+		switch (this) {
+		case APPLICANT_LIST:
+		case MANAGER_LIST:
+		case OFFICER_LIST:
+			return "Name,NRIC,Age,Marital Status,Password";
+		case PROJECT_LIST:
+			return "Project Name,Neighborhood,Type 1,Number of units for Type 1,Selling price for Type 1,Type 2,Number of units for Type 2,Selling price for Type 2,Application opening date,Application closing date,Manager,Officer Slot,Officer";
+		}
+	}
+	
 }
