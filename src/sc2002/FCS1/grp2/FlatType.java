6e4a2007238c9ed39ec2ac6b2b20c37d3ecac726
sadender <jji2804@naver.com>
2025-04-16 22:46:24 +0800

    Javadocs for FlatType

--- src/sc2002/FCS1/grp2/FlatType.java
+++ src/sc2002/FCS1/grp2/FlatType.java
@@ -4,20 +4,41 @@ package sc2002.FCS1.grp2;
  *	Enum that represents the type of the flats (The assignment assumes there are only 2-Room and 3-Room) 
  */
 public enum FlatType {
+	/**
+	 * Represents a 2-Room flat type.
+	 */
 	TWO_ROOM("2-Room"),
+
+	/**
+	 * Represents a 3-Room flat type.
+	 */
 	THREE_ROOM("3-Room");
 	
 	private String value;
 	
+	/**
+	 * Constructor for FlatType enum.
+	 * @param value The string representation of the FlatType.
+	 */
 	private FlatType(String value) {
 		this.value = value;
 	}
 	
+	/**
+	 * Get the string representation of the FlatType.
+	 * @return The string representation of the FlatType.
+	 */
 	@Override
 	public String toString() {
 		return value;
 	}
 	
+
+	/**
+	 * Convert a string to the respective FlatType enum.
+	 * @param value The string to convert.
+	 * @return The FlatType enum that matches the string, or null if no match is found.
+	 */
 	public static FlatType fromString(String value) {
 		for (FlatType type : FlatType.values()) {
 			if (type.value.equalsIgnoreCase(value)) {
